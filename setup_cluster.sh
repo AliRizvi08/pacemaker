f8f85a3664c03960f51010a87b041acbf21e5b49
AliRizvi08 <rizvi.ali.08@gmail.com>
2020-11-03 09:16:02 -0500

    ixth commit

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "changeme" | passwd --stdin hacluster
-pcs cluster auth Pacemaker1 Pacemaker2
-pcs cluster setup --start --name Pacemaker1 Pacemaker2
-pcs cluster enable --all
+#pcs cluster auth -u hacluster -p changeme Pacemaker1 Pacemaker2
+#pcs cluster setup --start --name Pacemaker Pacemaker1 Pacemaker2
+#pcs cluster enable --all
+pcs host auth -u hacluster -p changeme Pacemaker1 Pacemaker2
+pcs cluster setup Pacemaker Pacemaker1 Pacemaker2
+pcs cluster start --all
