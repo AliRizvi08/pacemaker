4ab0eebd1d905454b1ba61d5f9a3bcfc789fa03e
AliRizvi08 <rizvi.ali.08@gmail.com>
2020-11-03 09:30:01 -0500

    7th Update

--- ClusteringHASetup.sh
+++ ClusteringHASetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 bash hosts.sh
-#bash update_reboot.sh
+bash update.sh
 bash ntp_setup.sh
 bash pcs_fence_install.sh
 bash setup_cluster.sh
--- hosts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "10.5.0.2  Pacemaker1" >> /etc/hosts
-echo "10.5.0.3  Pacemaker2" >> /etc/hosts
